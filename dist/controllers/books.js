63b9cb5470854f3568da8b7fe226eea78f7675e3
m-yal <https://github.com/m-yal/mvc-books-library.git>
2022-10-06 16:28:56 +0300

    refactoring search method in books list controller

--- dist/controllers/books.js
+++ dist/controllers/books.js
@@ -24,6 +24,7 @@ function getBooks(req, res) {
     }
 }
 exports.getBooks = getBooks;
+;
 function getAll(req, res) {
     const offset = req.query.offset || 0;
     const sql = `SELECT * FROM books_v1 WHERE is_deleted = FALSE ORDER BY book_name ASC LIMIT ${LIMIT} OFFSET ${offset};`;
@@ -31,7 +32,7 @@ function getAll(req, res) {
         try {
             if (err)
                 throw err;
-            countBooksAmount(result, res, offset);
+            countBooksAmount(result, res, offset, null, sql, req);
         }
         catch (err) {
             yield res.status(500);
@@ -40,16 +41,17 @@ function getAll(req, res) {
     }));
 }
 ;
-function countBooksAmount(result, res, offset) {
-    const countSQL = `SELECT COUNT(*) AS count FROM books_v1 WHERE is_deleted = FALSE;`;
-    connection_1.default.query(countSQL, (err, rowsCount) => __awaiter(this, void 0, void 0, function* () {
+function countBooksAmount(result, res, offset, searchQuery, sql, req) {
+    const foundBooksCountSQLQuery = (typeof searchQuery === null) ?
+        `SELECT COUNT(*) AS count FROM books_v1 WHERE is_deleted = FALSE;`
+        : composeFoundBooksCountQuery(sql, `LIMIT ${LIMIT} OFFSET ${req.query.offset}`);
+    connection_1.default.query(foundBooksCountSQLQuery, (err, rowsCount) => __awaiter(this, void 0, void 0, function* () {
         try {
             if (err)
                 throw err;
             const pagesStatus = assemblePagesStatusData(offset, yield rowsCount[0].count);
-            console.log("pages Status: " + JSON.stringify(pagesStatus));
             yield res.status(200);
-            yield res.render("v1/books/index", { books: yield result, searchQuery: null, pagesStatus: pagesStatus });
+            yield res.render("v1/books/index", { books: yield result, searchQuery: searchQuery, pagesStatus: pagesStatus });
         }
         catch (err) {
             yield res.status(500);
@@ -68,14 +70,27 @@ function assemblePagesStatusData(offset, count) {
     return pagesStatus;
 }
 function search(req, res) {
-    console.log("INSIDE SEARCH METHOD");
-    const { author, year } = req.query;
+    const { author, year, search } = req.query;
     const offset = req.query.offset || 0;
-    const searchQuery = req.query.search;
+    const sql = composeSLQQuery(author, year, offset, search);
+    connection_1.default.query(sql, (err, result) => __awaiter(this, void 0, void 0, function* () {
+        try {
+            if (err)
+                throw err;
+            countBooksAmount(result, res, offset, search, sql, req);
+        }
+        catch (err) {
+            res.status(500);
+            return res.send({ error: "Error in database during searching books: " + err });
+        }
+    }));
+}
+;
+function composeSLQQuery(author, year, offset, searchQuery) {
+    let sql;
     const authorQuery = author ? `autor_id = ${author}` : "";
     const yearQuery = year ? `year = ${year}` : "";
     const offsetQuery = `LIMIT ${LIMIT} OFFSET ${offset}`;
-    let sql;
     if (!author && !year) {
         sql = `SELECT * FROM books_v1 WHERE is_deleted = FALSE AND book_name LIKE '%${searchQuery}%' ORDER BY book_name ASC ${offsetQuery};`;
     }
@@ -90,42 +105,11 @@ function search(req, res) {
             sql = `SELECT * FROM books_v1 WHERE is_deleted = FALSE AND book_name LIKE '%${searchQuery}%' AND ${yearQuery} ORDER BY book_name ASC ${offsetQuery};`;
         }
     }
-    console.log("sql " + sql);
-    connection_1.default.query(sql, (err, result) => __awaiter(this, void 0, void 0, function* () {
-        if (err) {
-            console.log(`Error during getting books`);
-            res.status(500);
-            return res.send({ error: "Error in database during searching books: " + err });
-        }
-        console.log(`Query result form file getting books: ${yield result}`);
-        console.log("search query: " + searchQuery);
-        const countSQL = sql.replace("*", "COUNT(*) AS count").replace("ORDER BY book_name ASC", "").replace(offsetQuery, "");
-        console.log("countSQL " + countSQL);
-        connection_1.default.query(countSQL, (err, rowsCount) => __awaiter(this, void 0, void 0, function* () {
-            if (err) {
-                console.log("Error during getting count of rows in talbe during getting all: " + err);
-                res.status(500);
-                return res.send({ error: "Error during getting count of rows in talbe during getting all: " + err });
-            }
-            let hasPrevPage = false, hasNextPage = false;
-            console.log("rowsCount " + rowsCount);
-            console.log("rowsCount[0] " + rowsCount[0]);
-            const totalyFound = rowsCount[0].count;
-            console.log("totalyFound " + totalyFound);
-            const offsetAhead = +offset + LIMIT;
-            console.log(`offsetAhead ${offsetAhead}`);
-            const offsetBack = +offset - LIMIT;
-            console.log(`offsetBack ${offsetBack}`);
-            if (offsetAhead <= totalyFound)
-                hasNextPage = true;
-            if (offsetBack >= 0)
-                hasPrevPage = true;
-            const pagesStatus = { hasPrevPage: hasPrevPage, hasNextPage: hasNextPage, totalyFound: totalyFound, offsetAhead: offsetAhead, offsetBack: offsetBack };
-            console.log(`hasPrevPage ${hasPrevPage}`);
-            console.log(`hasNextPage ${hasNextPage}`);
-            yield res.status(200);
-            yield res.render("v1/books/index", { books: yield result, searchQuery: searchQuery, pagesStatus: pagesStatus });
-        }));
-    }));
+    return sql;
 }
-;
+function composeFoundBooksCountQuery(sql, offset) {
+    return sql
+        .replace("*", "COUNT(*) AS count")
+        .replace("ORDER BY book_name ASC", "")
+        .replace(offset, "");
+}
